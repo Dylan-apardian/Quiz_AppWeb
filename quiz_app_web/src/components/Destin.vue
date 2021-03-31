3770f7474086a8756b2bc827be2770da2af89d4f
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2021-03-31 17:49:01 -0400

    component marche

---
+++ quiz_app_web/src/components/Destin.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Destin',
+  data () {
+    return {
+      msg: 'Bonjour le gang'
+    }
+  }
+}
+</script>
+
