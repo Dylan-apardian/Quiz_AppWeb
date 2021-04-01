f69829ee063a444faf45774899b32f1043c17d21
Dorensky <64221942+Dorensky@users.noreply.github.com>
2021-04-01 17:01:00 -0400

    header

--- quiz_app_web/src/App.vue
+++ quiz_app_web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header/>
     <HelloWorld/>
     <Destin/>
   </div>
@@ -9,8 +9,9 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Destin from './components/Destin.vue'
+import Header from './components/Header.vue'
 export default {
-  components: {HelloWorld, Destin},
+  components: {HelloWorld, Destin, Header},
   name: 'App'
 }
 </script>
@@ -22,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
