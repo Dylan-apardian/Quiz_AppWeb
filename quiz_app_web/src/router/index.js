d26126c64439f58d2e30eef9c05fa6e7038ce180
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2021-04-01 16:05:14 -0400

    nouvaux components

--- quiz_app_web/src/router/index.js
+++ quiz_app_web/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-//import Destin from '@/components/Destin'
 import HelloWorld from '@/components/HelloWorld'
-
 
 Vue.use(Router)
 
@@ -11,9 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld 
-      
+      component: HelloWorld
     }
   ]
 })
-
