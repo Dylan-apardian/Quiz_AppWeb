be3e03cf7409c6293732a9177a191e1e80649238
Dorensky <64221942+Dorensky@users.noreply.github.com>
2021-04-01 23:36:05 -0400

    fini accueil

--- quiz_app_web/src/components/Accueil.vue
+++ quiz_app_web/src/components/Accueil.vue
@@ -1,23 +1,101 @@
 <template>
-  <div class="Accueil">
-      <app-header></app-header>
-      <h1>Accueil</h1>
+  <div class="Accueil" :style="{backgroundImage: `url(${backgroundUrl}) ` }">
+    <app-header></app-header>
+    <h2>{{message}}</h2>
+                <router-link to="destination" id="orange">En savoir plus</router-link>
+
+        <footer>
+            <h1>{{nomSite}}</h1>
+            <div id="copyright">{{copyright}}</div>
+        </footer>
   </div>
 </template>
 
 <script>
-// import your components here
+import backgroundUrl from '../assets/paysage.jpg'
 import Header from './Header.vue'
 export default {
+  name: 'Header',
+  data () {
+    return {
+      backgroundUrl,
+      nomSite: 'DhafVoyage', 
+      message: 'Explorez le meilleur site de tourisme jamais conçu',
+      copyright: 'Copyright © 2021. Tous droits réservés.'
+    };
+  },
   components: {
     'app-header': Header
-  }
+  } 
 }
 </script>
 
 <style scoped>
+* {
+  margin: 10 auto;
+  padding: 0 10px;
+}
+
 .Accueil {
   display: flex;
   flex-direction: column;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: no-repeat center;
+  background-size:cover;
+  height: 789px;
+  width: 1580px;
+}
+
+h2 {
+    font-weight: normal;
+    font-size: 32px;
+    text-transform: uppercase;
+    text-align: center;
+    padding: 150px 0 40px 0;
+    margin: 20px;
+}
+
+#orange {
+    display: block;
+    width: 130px;
+    height: 50px;
+    background: #ff7a00;
+    color: white;
+    font-size: 20px;
+    margin: 0 auto;
+    line-height: 50px;
+    text-align: center;
+    border-radius: 3px;
+}
+
+#orange:hover {
+    background: #02b8dd;
+}
+
+footer
+{
+    height: 180px;
+    background-color:lightgray;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-style:solid;
+}
+
+footer h1
+{
+    text-align: center;
+    padding-top: 40px;
+}
+
+.copyright
+{
+    text-align: center;
+    font-weight: bold;
+    padding-top: 30px;
 }
 </style>
--- quiz_app_web/src/components/Header.vue
+++ quiz_app_web/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="entete" :style="{backgroundImage: `url(${backgroundUrl}) ` }">  
+    <div class="entete">
         <header>
-            <h1>{{nomSite}}</h1>
+            <h1>{{page.nomSite}}</h1>
                 <nav>
                     <ul>
                         <li><router-link to="/">{{page.accueil}}</router-link></li>
@@ -9,104 +9,33 @@
                         <li><router-link to="reviews">{{page.reviews}}</router-link></li>
                         <li><router-link to="apropos">{{page.aPropos}}</router-link></li>
                     </ul>
-                </nav> 
-        </header>    
-        
-        <h2>{{message}}</h2>
-                <a>En savoir plus</a>
-
-        <footer>
-            <h1>{{nomSite}}</h1>
-            <div id="copyright">{{copyright}}</div>
-        </footer>
+                </nav>
+        </header>
     </div>
 </template>
 
 <script>
-import backgroundUrl from "../assets/paysage.jpg";
 export default {
   name: 'Header',
   data () {
     return {
-      backgroundUrl,
       page: {
-          accueil:'Accueil',
-          destinations:'Destinations',
-          reviews:'Reviews',
-          aPropos:'À Propos',
-      },
-      message: 'Explorez le meilleur site de tourisme jamais conçu',
-      nomSite: 'DhafVoyage',
-      copyright: 'Copyright © 2021. Tous droits réservés.'
+        nomSite: 'DhafVoyage',  
+        accueil: 'Accueil',
+        destinations: 'Destinations',
+        reviews: 'Reviews',
+        aPropos: 'À Propos'
+      }
     }
   }
 }
 </script>
 
- <!-- <div class="entete">
-    <header>
-      <h1>DhafVoyage</h1>
-      <nav>
-        <ul>
-          <li><router-link to="/">Accueil</router-link></li>
-          <li><router-link to="destination">Destinations</router-link></li>
-          <li><router-link to="reviews">Reviews</router-link></li>
-          <li><router-link to="apropos">A propos</router-link></li>
-        </ul>
-      </nav>
-    </header>
-  </div>
-</template> -->
-
 <style scoped>
-* {
-  margin: 10 auto;
-  padding: 0 10px;
-}
-
-.entete {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    background: no-repeat center;
-    background-size:cover;
-    height: 789px;
-    width: 1580px;
-}
 header h1 {
      float: left;
      margin-top: 20px;
      font-size: 40px;
-}
-
-h2 {
-    font-weight: normal;
-    font-size: 32px;
-    text-transform: uppercase;
-    text-align: center;
-    padding: 150px 0 40px 0;
-    margin: 70px;
-    
-}
-
-a {
-    display: block;
-    width: 140px;
-    height: 50px;
-    background: #ff7a00;
-    color: white;
-    font-size: 20px;
-    margin: 0 auto;
-    line-height: 50px;
-    text-align: center;
-    border-radius: 3px;
-}
-
-a:hover {
-    background: #02b8dd;
 }
 
 nav {
@@ -121,27 +50,4 @@ nav ul li {
   margin-right: 80px;
 }
 
-footer
-{
-    height: 180px;
-    background-color:lightgray;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    border-style:solid;
-}
-
-footer h1
-{
-    text-align: center;
-    padding-top: 40px;
-}
-
-.copyright
-{
-    text-align: center;
-    font-weight: bold;
-    padding-top: 30px;
-}
 </style>
