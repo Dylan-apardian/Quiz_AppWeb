13c024c71dee2385f3961902b41dd49ee04e86ac
Dylan-apardian <1947376@bdeb.qc.ca>
2021-04-01 22:30:32 -0400

    page a propos avec images

--- quiz_app_web/src/components/Apropos.vue
+++ quiz_app_web/src/components/Apropos.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="Apropos">
       <app-header></app-header>
-      <h1>A propos</h1>
+      <div class="contenu">
+        <h1 class='titre'>Notre mission</h1>
+        <h2 class="desc">Offrir des voyages à nos clients au meilleur prix sur le marché</h2>
+        <h1 class='titre'>A propos de nous</h1>
+        <h2 class="desc">Depuis 2012, DhafVoyage est leader mondial du marché des agences de voyages et de tourisme. Nous nous efforçons de satisfaire nos clients lors de leurs déplacements. Le client vient toujours en premier pour nous. Lorsque vous voyagez avec nous, nous garantissons votre satisfaction sinon vous êtes remboursées.</h2>
+        <h1 class='titre'>Nos clients satisfaits</h1>
+        <div class="photos">
+            <img class="image" src="../assets/famille1.png"/>
+            <img class="image" src="../assets/famille2.png"/>
+            <img class="image" src="../assets/famille3.png"/>
+        </div>
+        <div class="photos">
+            <img class="image" src="../assets/famille4.png"/>
+            <img class="image" src="../assets/famille5.png"/>
+            <img class="image" src="../assets/famille6.png"/>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-// import your components here
 import Header from './Header.vue'
 export default {
   components: {
@@ -19,5 +34,32 @@ export default {
 .Apropos {
   display: flex;
   flex-direction: column;
+}
+.contenu{
+    align-self: center;
+    background-color: rgb(231, 231, 231);
+    margin-left: 10%;
+    margin-right: 10%;
+    max-width: 1000px;
+    display: flex;
+    flex-direction: column;
+}
+.titre{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.desc{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: 400;
+}
+.photos{
+    margin-bottom: 5%;
+    align-self: center;
+    display: flex;
+    flex-direction: row;
+}
+.image{
+    width: 300px;
+    height: auto;
+    padding: 10px;
 }
 </style>
